fcc01b5fc613b2f94eb1d6f2a7df13573cb684da
pechera <romankotliar7@gmail.com>
2023-04-23 17:24:26 +0300

    add mobile detect

--- email.php
+++ email.php
@@ -1,24 +1,72 @@
 <?php
-// --------------------------------
-$to = "{{email}}";
+
+require_once "./lib/Mobile_Detect.php";
+date_default_timezone_set('Europe/Rome');
+
+// SETUP --------------------------
+$from = 'order@youshop.com';
+$to = "";
+$redirect = 'thankyou.php';
 // --------------------------------
 
-$postData = $_POST;
-$domain = $_SERVER['SERVER_NAME'];
+$params = array();
+
+// GET DATA
+
+foreach ($_POST as $name => $value) {
+  $params[$name] = $value;
+}
+
+function get_client_ip() {
+  $ip = getenv('REMOTE_ADDR') ? getenv('REMOTE_ADDR') : '8.8.8.8';
+  return $ip;
+}
+
+$detect = new Mobile_Detect;
+$detect->isMobile() ? $params['isMobile'] = 'MobileDevice' : $params['isMobile'] = 'DesktopPC';
+
+$params['ip'] = get_client_ip();
+$params['curr_date_time'] = date('Y-m-d H:i:s', time());
+$params['referer'] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+
+$ch = curl_init();
+$content = '';
+if ($ch) {
+	curl_setopt_array($ch, array(
+	    CURLOPT_URL => 'http://ip-api.com/json/'.$params['ip'],
+	    CURLOPT_RETURNTRANSFER => true,
+	    CURLOPT_ENCODING => "",
+	    CURLOPT_MAXREDIRS => 10,
+	    CURLOPT_TIMEOUT => 30,
+	    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	    CURLOPT_CUSTOMREQUEST => "GET",
+	));
+    $content = trim(curl_exec($ch));
+    curl_close($ch);
+}
+
+$arr = json_decode($content, true);
+
+$params['city'] = $arr['city'];
+$params['country'] = $arr['country'];
+$params['countryCode'] = $arr['countryCode'];
+$params['regionName'] = $arr['regionName'];
+
+// SEND EMAIL
+
+$headers = "From: " . $from . "\r\n";
+$headers .= "Reply-To: " . $from . "\r\n";
+$headers .= "X-Mailer: PHP/".phpversion();
 
 $message = "";
-foreach ($postData as $name => $value) {
+foreach ($params as $name => $value) {
   $message .= "$name: $value\n";
 }
 
-$headers = "From: order@pagesaver.click\r\n";
-$headers .= "Reply-To: order@pagesaver.click\r\n";
-$headers .= "X-Mailer: PHP/".phpversion();
+mail($to, "New lead from " . $params['referer'], $message, $headers);
 
-mail($to, "New lead from " . $domain, $message, $headers);
+// REDIRECT
 
-header('Location: thankyou.php');
-
-exit; 
+header('Location: '. $redirect);
 
 ?>
--- webhook.php
+++ webhook.php
@@ -1,29 +1,69 @@
 <?php
 
-// --------------------------------
-$url = "{{url}}";
+require_once "./lib/Mobile_Detect.php";
+date_default_timezone_set('Europe/Rome');
+
+// SETUP --------------------------
+$url = "";
+$redirect = 'thankyou.php';
 // --------------------------------
 
-$data = $_POST;
-$queryString = http_build_query($data);
+$params = array();
+
+foreach ($_POST as $name => $value) {
+    $params[$name] = $value;
+}
+
+function get_client_ip() {
+    $ip = getenv('REMOTE_ADDR') ? getenv('REMOTE_ADDR') : '8.8.8.8';
+    return $ip;
+}
+  
+$detect = new Mobile_Detect;
+$detect->isMobile() ? $params['isMobile'] = 'MobileDevice' : $params['isMobile'] = 'DesktopPC';
+  
+$params['ip'] = get_client_ip();
+$params['curr_date_time'] = date('Y-m-d H:i:s', time());
+$params['referer'] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+  
+$ch = curl_init();
+$content = '';
+if ($ch) {
+    curl_setopt_array($ch, array(
+        CURLOPT_URL => 'http://ip-api.com/json/'.$params['ip'],
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+    ));
+    $content = trim(curl_exec($ch));
+    curl_close($ch);
+}
+  
+$arr = json_decode($content, true);
+  
+$params['city'] = $arr['city'];
+$params['country'] = $arr['country'];
+$params['countryCode'] = $arr['countryCode'];
+$params['regionName'] = $arr['regionName'];
+
+// SEND WEBHOOK
+
+$queryString = http_build_query($params);
 
 $webhookUrl = "";
-if (strpos($string, '?') !== false) {
-    $webhookUrl = $url . $queryString;
-} else {
-    $webhookUrl = $url . '?' . $queryString;
-}
+strpos($url, '?') ? $webhookUrl = $url . '&' . $queryString : $webhookUrl = $url . '?' . $queryString;
 
 $response = file_get_contents($webhookUrl);
 
 $status = http_response_code();
 
 if ($status == 200) {
-    header('Location: thankyou.php');
+     header('Location: ' . $redirect);
 } else {
     echo "Error. Try again!";
 }
-
-exit; 
 
 ?>
